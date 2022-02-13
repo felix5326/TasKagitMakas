30e16dfaee21ac6b47e26b8674580de5fb0842cb
timmy <81864321+timmythesheep1@users.noreply.github.com>
2022-02-14 01:03:38 +0300

    Update main.py

--- 1.0/main.py
+++ 1.0/main.py
@@ -5,7 +5,7 @@ choice=["stone","paper","scissors"]
 stone=choice[0]
 paper=choice[1]
 scissors=choice[2]
-print("Hello, if you want exit click the.
+print("""Hello, if you want exit click the.
 1 - Play Game
 2 - Information Game
-3 - Exit")
+3 - Exit""")
