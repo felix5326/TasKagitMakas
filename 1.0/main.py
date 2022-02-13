6dd0217c40a1ab31c951c28a12ada8b384d44d74
timmy <81864321+timmythesheep1@users.noreply.github.com>
2022-02-14 01:02:06 +0300

    Update main.py

--- 1.0/main.py
+++ 1.0/main.py
@@ -1,8 +1,12 @@
 import random
 
 #veriables
-choice=["stone","paper","scissors"]
-stone=choice[0]
-paper=choice[1]
-scissors=choice[2]
-print("Hello, if you want exit click the.")
+#choice=["stone","paper","scissors"]
+#stone=choice[0]
+#paper=choice[1]
+#scissors=choice[2]
+print("Hello, if you want exit click the.
+1 - Play Game
+2 - Information Game
+3 - Exit
+")
